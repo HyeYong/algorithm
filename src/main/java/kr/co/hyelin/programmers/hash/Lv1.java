37e08a81af22fe9ea979a4a038c35becb517d8e1
김혜린 <sjgpfls@gmail.com>
2021-09-08 23:17:11 +0900

    🎨 style : programmers - 코딩테스트연습 - hash - 전화번호 목록

--- src/main/java/kr/co/hyelin/programmers/hash/Lv1.java
+++ src/main/java/kr/co/hyelin/programmers/hash/Lv1.java
@@ -4,38 +4,38 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Lv1 {
-    public static String solution(String[] participant, String[] completion) {
+	public static String solution(String[] participant, String[] completion) {
 
-        /* type 1 : Array */
-//        Arrays.sort(participant);
-//        Arrays.sort(completion);
-//
-//        int i;
-//
-//        for(i=0; i<completion.length; i++) {
-//            if(!participant[i].equals(completion[i]))
-//                return participant[i];
-//        }
-//
-//        return participant[i];
+		/* type 1 : Array */
+		//        Arrays.sort(participant);
+		//        Arrays.sort(completion);
+		//
+		//        int i;
+		//
+		//        for(i=0; i<completion.length; i++) {
+		//            if(!participant[i].equals(completion[i]))
+		//                return participant[i];
+		//        }
+		//
+		//        return participant[i];
 
 
-        /* type 2 : HashMap */
-        HashMap<String, Integer> participantHash = new HashMap<>();
+		/* type 2 : HashMap */
+		HashMap<String, Integer> participantHash = new HashMap<>();
 
-        for(String player : participant) {
-            participantHash.put(player, participantHash.getOrDefault(player, 0) + 1);
-        }
-        for(String player : completion) {
-            participantHash.put(player, participantHash.get(player) - 1);
-        }
+		for (String player : participant) {
+			participantHash.put(player, participantHash.getOrDefault(player, 0) + 1);
+		}
+		for (String player : completion) {
+			participantHash.put(player, participantHash.get(player) - 1);
+		}
 
-        for(String key : participantHash.keySet()) {
-            if(participantHash.get(key) != 0) {
-                return key;
-            }
-        }
+		for (String key : participantHash.keySet()) {
+			if (participantHash.get(key) != 0) {
+				return key;
+			}
+		}
 
-        return "";
-    }
+		return "";
+	}
 }
--- src/main/java/kr/co/hyelin/programmers/hash/Lv2.java
+++ src/main/java/kr/co/hyelin/programmers/hash/Lv2.java
@@ -5,32 +5,32 @@ import java.util.HashSet;
 
 public class Lv2 {
 
-    public static boolean solution(String[] phone_book) {
+	public static boolean solution(String[] phone_book) {
 
-        /* type 1 : HashSet */
-//        HashSet<String> phoneBookSet = new HashSet<>(Arrays.asList(phone_book));
-//
-//        for(String phoneNumber : phoneBookSet) {
-//            for (int i = 1; i < phoneNumber.length(); i++) {
-//                if(phoneBookSet.contains(phoneNumber.substring(0, i))) {
-//                    return false;
-//                }
-//            }
-//        }
-//
-//        return true;
+		/* type 1 : HashSet */
+		//        HashSet<String> phoneBookSet = new HashSet<>(Arrays.asList(phone_book));
+		//
+		//        for(String phoneNumber : phoneBookSet) {
+		//            for (int i = 1; i < phoneNumber.length(); i++) {
+		//                if(phoneBookSet.contains(phoneNumber.substring(0, i))) {
+		//                    return false;
+		//                }
+		//            }
+		//        }
+		//
+		//        return true;
 
 
-        /* type 2 : Array */
-        Arrays.sort(phone_book);
+		/* type 2 : Array */
+		Arrays.sort(phone_book);
 
-        for(int i=0; i<phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
-            }
-        }
+		for (int i = 0; i < phone_book.length - 1; i++) {
+			if (phone_book[i + 1].startsWith(phone_book[i])) {
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
 }
